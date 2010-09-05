3d6e042e8718bce53ca84f9ce2fd381b5eeeebf4
chuminhtri@gmail.com <chuminhtri@gmail.com@a7811d78-34aa-4512-2aaf-9c23cbf1bc95>
2010-09-05 01:43:03 +0000

    finish homework 1

--- HW1/src/GzFrameBuffer.cpp
+++ HW1/src/GzFrameBuffer.cpp
@@ -1,28 +1,80 @@
 #include "GzFrameBuffer.h"
 
 //Put your implementation here------------------------------------------------
+
+
+
 void GzFrameBuffer::initFrameSize (GzInt width, GzInt height)
 {
-	width_ = width;
-	height_ = height;
+	Width = width;
+	Height = height;
+	bgColor = GzColor(0,0,0);
+	defDepth = 0;
+	Color_Buffer = new GzColor[Width*Height];
+	Depth_Buffer = new GzReal[Width*Height];
+
 }
 
 GzImage GzFrameBuffer::toImage()
 {
+	GzImage image(Width,Height);
+	for(int j = 0; j <Height;j++)
+			for(int i = 0; i < Width;i++)
+				image.set(i,j,Color_Buffer[idx2D_1D(i,j,Width)]);
+
+	return image;
 }
 
 void GzFrameBuffer::clear(GzFunctional buffer)
 {
+	if( buffer & GZ_COLOR_BUFFER )
+	{
+		for(int j = 0; j <Height;j++)
+			for(int i = 0; i < Width;i++)
+				Color_Buffer[idx2D_1D(i,j,Width)] = bgColor;
+	}
+
+	if ( buffer & GZ_DEPTH_BUFFER )
+	{
+		for(int j = 0; j <Height;j++)
+			for(int i = 0; i < Width;i++)
+				Depth_Buffer[idx2D_1D(i,j,Width)] = defDepth;
+	}
 }
 
 void GzFrameBuffer::setClearColor(const GzColor& color)
 {
+	bgColor = color;
 }
 
 void GzFrameBuffer::setClearDepth(GzReal depth)
 {
+	defDepth = depth;
 }
 
 void GzFrameBuffer::drawPoint(const GzVertex& v, const GzColor& c, GzFunctional status)
 {
+	
+	int VXCoord = round(v[X]);
+	//cout << VXCoord << endl;
+	int VYCoord = -round(v[Y])+Height-1;
+	//cout << VYCoord << endl;
+	int VZCoord = round(v[Z]);
+	//cout << VZCoord << endl;
+
+	if(!checkBound(VXCoord,VYCoord,Width,Height))
+		return;
+
+	if (status & GZ_DEPTH_TEST)
+	{
+		if(VZCoord > Depth_Buffer[idx2D_1D(VXCoord,VYCoord,Width)])
+		{
+			Depth_Buffer[idx2D_1D(VXCoord,VYCoord,Width)] = VZCoord;
+			Color_Buffer[idx2D_1D(VXCoord,VYCoord,Width)] = c;
+		}
+	}
+	else
+	{
+		Color_Buffer[idx2D_1D(VXCoord,VYCoord,Width)] = c;
+	}
 }
