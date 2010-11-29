e79bb61505c4e69e1560e14ce74df2dc4280953a
chuminhtri@gmail.com <chuminhtri@gmail.com@a7811d78-34aa-4512-2aaf-9c23cbf1bc95>
2010-11-29 14:32:09 +0000

    added the vec3f class. Ready to do some collision detection. Must write the occ tree too.

--- trunk/teamProject/src/main.cpp
+++ trunk/teamProject/src/main.cpp
@@ -1,5 +1,17 @@
 #include <QtCore/QCoreApplication>
 
+#include <cstdlib>
+#include <ctime>
+#include <iostream>
+#include <set>
+#include <stdlib.h>
+#include <vector>
+
+#include <GL/glut.h>
+#include <GL.h>
+
+using namespace std;
+
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
