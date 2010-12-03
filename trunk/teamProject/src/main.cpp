956ff260be6354e81a9271d0261d4e304f638d02
chuminhtri@gmail.com <chuminhtri@gmail.com@a7811d78-34aa-4512-2aaf-9c23cbf1bc95>
2010-12-03 04:44:00 +0000

    add the project into visual studio and setting up glut32. Ready to develop.

--- trunk/teamProject/src/main.cpp
+++ trunk/teamProject/src/main.cpp
@@ -1,3 +1,84 @@
+<<<<<<< .mine
+
+
+#include <cstdlib>
+#include <ctime>
+#include <iostream>
+#include <set>
+#include <stdlib.h>
+#include <vector>
+#include "glut.h"
+
+#ifdef __APPLE__
+#include<OpenGL/OpenGL.h>
+#include<GLUT/glut.h>
+#else
+#include <gl/glu.h>
+#endif
+
+using namespace std;
+
+
+void handleKeypress(unsigned char key, int x, int y)
+{
+	switch(key){
+	case 27: //Escape key
+		exit(0);
+	}
+}
+
+void initRendering()
+{
+	glEnable(GL_DEPTH_TEST);
+}
+
+void handleResize(int w, int h)
+{
+	glViewport(0,0,w,h);
+	
+	glMatrixMode(GL_PROJECTION);
+
+	glLoadIdentity();
+
+	gluPerspective(45.0,(double)w / (double)h,1.0,200.0);
+
+}
+
+
+void drawScene()
+{
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+	glMatrixMode(GL_MODELVIEW);
+	glLoadIdentity();
+
+	glBegin(GL_QUADS);
+
+	glEnd();
+
+	glutSwapBuffers();
+}
+int main(int argc, char** argv)
+{
+	glutInit(&argc, argv);
+	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
+	glutInitWindowSize(640, 480); //Set the window size
+	
+	//Create the window
+	glutCreateWindow("Team project - collisions");
+	initRendering(); //Initialize rendering
+	
+	//Set handler functions for drawing, keypresses, and window resizes
+	glutDisplayFunc(drawScene);
+	glutKeyboardFunc(handleKeypress);
+	glutReshapeFunc(handleResize);
+	
+	glutMainLoop(); //Start the main loop.  glutMainLoop doesn't return.
+	
+
+    return 0;
+}
+=======
 #include <QtCore/QCoreApplication>
 
 #include <cstdlib>
@@ -18,3 +99,4 @@ int main(int argc, char *argv[])
 
     return a.exec();
 }
+>>>>>>> .r45
