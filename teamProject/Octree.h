3627d9d12f021b5764a62e3023457167ee49abf2
chuminhtri@gmail.com <chuminhtri@gmail.com@a7811d78-34aa-4512-2aaf-9c23cbf1bc95>
2010-12-08 09:07:20 +0000

    look like the octree is settled. Now I need to write the main program. If all doesn't work by evening today, I will have to copy and paste the example code.

--- teamProject/Octree.h
+++ teamProject/Octree.h
@@ -4,6 +4,7 @@
 #include <cstdlib.h>
 #include <cmath>
 #include <vector>
+#include <stack>
 
 #include "Particle.h"
 #include "src\vec3f.h"
@@ -11,70 +12,42 @@
 using namespace std;
 
 
-int maxDepth = 10;
-int minNumberOfParticle = 3;
-int maxNumberOfParticle = 6;
 
-class Octree
-{
-private:
-	Vec3f corner1; //(minX, minY, minZ)
-	Vec3f corner2; //(maxX, maxY, maxZ)
-	Vec3f center;//((minX + maxX) / 2, (minY + maxY) / 2, (minZ + maxZ) / 2)
-	Vec3f halfWidth;//(corner2 - corner1)/2
-
-	Octree *pChild[8];
-	//Is this a leaf node
-	bool hasChildren;
-	//List of Particle inside of this node
-	vector<Particle*> pParticle;
-	//The depth of this node in the tree
-	int depth;
-	
-	//Clear all children nodes of the tree root at this node and add all their
-	//Particles set into the node particles set
-	void clearNode();
-	
-	//Add all particle pointer in the vector of particle store in the sub tree root
-	//at this node to the vector in param
-	void collectParticles(vector<Particle*> &pv);
-
-	//Number of particle obj stored in the sub-tree root at this node
-	int pCount;
-
-	//Figure out which sub-tree of the current node does the current ball go into
-	void branch (Particle* p, int &index, bool& straddle);
-
-	void testParticlesCollision(Octree *pTree,vector<particleCollision> &cs);
-
-public:
-
-	
-	Octree(int d, Vec3f c1, Vec3f c2);
-	~Octree(void);
-
-	//Add Particle into this tree
-	void add(Particle* particle);
-	//Remove a Particle from this tree
-	void remove(Particle* particle);
-	
-	//Move a particle around the tree if needed
-	//@param p: the particle with the new information
-	//@param oldPos: previous world position used to compute tree position
-	//void move (Particle* p, Vec3f oldPos);
-
-	//Test particles collision
-	//params cs: the list of particle collision we will use to paste the
-	//collisions into
-	void testParticlesCollision(vector<particleCollision> &cs);
-
-	//Test particles collision with walls
-	//@param cs: list of collision to paste back to collision handler
-	//@param w: wall object, present the wall we want to test with.
-	void testWallCollision(vector<wallCollision> &cs, Wall w);
-	
-	//rebuild the tree because the balls position has changed
-	void rebuild()
+struct Object {
+	Particle* particle;
+	Object *pNextObject; // Pointer to next object when linked into list
 };
+
+
+// Octree node data structure
+struct Node {
+	Vec3f center; // Center point of octree node 
+	float halfWidth; // Half the width of the node volume 
+	Node *pChild[8]; // Pointers to the eight children nodes
+	Object *pObjList; // Linked list of objects contained at this node
+};
+
+// Preallocates an octree down to a specific depth
+Node *BuildOctree(Vec3f center, float halfWidth, int stopDepth);
+
+//inserting an object to a pre-defined octree
+void InsertObject(Node *pTree, Object *pObject);
+
+// Tests all objects that could possibly overlap due to cell ancestry and coexistence
+// in the same cell. Assumes objects exist in a single cell only, and fully inside it
+void TestParticleCollision(Node *pTree, stack<ParticleCollision> &pStack);
+
+//Test collision between two object
+bool TestCollision(Object *pA, Object *pB);
+
+//Test all particle box wall collisions
+void TestWallCollision(Node *pTree, stack<WallCollision> &collisions);
+
+void potentialWallCollisions(Node *pTree,stack<WallCollision> &collisions,
+										 Wall w,char coord, int dir);
+
+
+
+
 
 #endif
